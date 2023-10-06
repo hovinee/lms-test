af9092fc17528e53d1a928c903cc35507c5a0a3e
hobinyoo <hbyoo@ducowise.com>
2023-10-06 17:24:26 +0900

    first-commit

---
+++ src/layouts/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between w-full px-10 py-3 bg-yellow-200">
+      <div className="text-red-500">로고</div>
+      <div className="text-red-500">햄버거</div>
+    </div>
+  )
+}
+
+export default Header
