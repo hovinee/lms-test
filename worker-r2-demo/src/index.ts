ffa9165dd4e74caf0c6c90fe12568020c996ed3c
DESKTOP-99VTEOR\hobin <hbyoo@ducowise.com>
2023-10-16 09:17:35 +0900

    Initial commit (by create-cloudflare CLI)

--- worker-r2-demo/src/index.ts
+++ worker-r2-demo/src/index.ts
@@ -8,25 +8,39 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+import handleProxy from './proxy';
+import handleRedirect from './redirect';
+import apiRouter from './router';
 
+// Export a default object containing event handlers
 export default {
+	// The fetch handler is invoked when this worker receives a HTTP(S) request
+	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
+		const url = new URL(request.url);
+
+		// You can get pretty far with simple logic like if/switch-statements
+		switch (url.pathname) {
+			case '/redirect':
+				return handleRedirect.fetch(request, env, ctx);
+
+			case '/proxy':
+				return handleProxy.fetch(request, env, ctx);
+		}
+
+		if (url.pathname.startsWith('/api/')) {
+			// You can also use more robust routing
+			return apiRouter.handle(request);
+		}
+
+		return new Response(
+			`Try making requests to:
+      <ul>
+      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
+      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
+      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
+			{ headers: { 'Content-Type': 'text/html' } }
+		);
 	},
 };
