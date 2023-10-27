d8f1d051005b4625c853448f0483e4ae4d94b3de
Park KyeongKwon <team3@ducogen.net>
2023-10-27 11:54:41 +0900

    chore & refac: 오탈자 & import 경로 변경

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,12 @@
 import LoginForm from '@components/form/LoginForm'
-import { authOptions } from 'app/api/auth/[nextauth]'
+import { authOptions } from 'app/api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
 import { signIn } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 
 const Login = async () => {
   const session = await getServerSession(authOptions)
-  if (session) redirect('/auth/userInfo')s
+  if (session) redirect('/auth/userInfo')
   return (
     <div className="flex h-[calc(100vh-5.6rem)] w-full items-center">
       <LoginForm />
